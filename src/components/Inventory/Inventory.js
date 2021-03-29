234a6dc553e43271a414d7a7506fd9004dad3eb0
shajjadkabir <sajjadkabir71@gmail.com>
2021-03-29 15:23:49 +0600

    mongodb work added

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -1,9 +1,27 @@
 import React from 'react';
 
 const Inventory = () => {
+    const product = {};
+    const handleAddProduct = () => {
+        fetch('http://localhost:5000/addProduct', {
+            method: 'POST',
+            headers:
+            {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(product)
+        })
+    }
     return (
         <div>
-            <h1>Inventory coming soon...</h1>
+            <form action="">
+              <p><span>Name: </span><input type="text"/></p>
+              <p><span>Price: </span><input type="text"/></p>
+              <p><span>Quantity: </span><input type="text"/></p>
+              <p><span>product image: </span><input type="file"/></p>
+                <button onClick={handleAddProduct}>Add Product</button>
+            </form>
+            
         </div>
     );
 };
